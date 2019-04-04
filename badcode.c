eb1fa5449f367eab90a456fcda13ab3ebc12d27c
sasa2009 <salfarees@worcester.edu>
2019-04-04 17:21:40 -0400

    These are the Intermediate changes Please enter the commit message for your changes. Lines starting

--- badcode.c
+++ badcode.c
@@ -5,35 +5,49 @@
 
 int main() {
 
-   const float R = 6371.01;
+   const float Radius = 6371.01;
    const float PI = 3.141592654;
-   float s_lat, s_long, e_lat, e_long, D;
+   float start_lat, start_long, end_lat, end_long, Distance;
    char choice;
    do
    {
 
-            printf(" Please enter the latitude and longitude : \n");
-            printf(" Input s_lat: ");
-            scanf("%f", &s_lat);
-            s_lat = (s_lat)*(PI/180);
-            printf(" Input s_long: ");
-            scanf("%f", &s_long);
-            s_long = (s_long)*(PI/180);
-            printf(" Input e_lat: ");
-            scanf("%f", &e_lat);
-            e_lat = (e_lat)*(PI/180);
-            printf(" Input e_long: ");
-            scanf("%f", &e_long);
-            e_long = (e_long) *(PI/180);
-       printf("\n Your points after converting them to radians are:\n s_lat = %f  \n s_long = %f \n e_lat = %f \n e_long = %f \n",s_lat, s_long, e_lat, e_long);
-       D = R*acos(sin(s_lat)*sin(e_lat) + cos(s_lat)*cos(e_lat)*cos(s_long - e_long));
+            printf(" This program calculates the distance between two places on Earth: \n");
+
+
+            printf(" Please enter the latitude and longitude of two places: \n");
+            printf(" Input Start Latitude: ");
+            scanf("%f", &start_lat);
+
+            start_lat = (start_lat)*(PI/180);
+            printf(" Input Start Longitude: ");
+            scanf("%f", &start_long);
+
+            start_long = (start_long)*(PI/180);
+            printf(" Input End Latitude: ");
+            scanf("%f", &end_lat);
+
+            end_lat = (end_lat)*(PI/180);
+            printf(" Input End Longitude: ");
+            scanf("%f", &end_long);
+
+            end_long = (end_long) *(PI/180);
+
+
+    printf("\n Your points after converting them to radians are:\n start_lat = %f  \n start_long = %f \n end_lat = %f \n end_long = %f \n",start_lat, start_long, end_lat, end_long);
+
+    Distance = Radius*acos(sin(start_lat)*sin(end_lat) + cos(start_lat)*cos(end_lat)*cos(start_long - end_long));
+
             printf("\n");
             printf(" Distance: //%.2f", D);
+
             printf(" km");
             printf("\n\n");
+
             printf(" If you want to continue press (Y) or (y), if you want to stop, press any key ");
             fflush(stdin);
             scanf("%c", &choice);
+
    } while((choice == 'y')||(choice == 'Y'));
     printf("\n\n");
     printf(" Thank You For Using The Program ");
